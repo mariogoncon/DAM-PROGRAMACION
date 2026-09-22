83caa2c53ce6163046d2a259baa302451e997bc5
Mario <mariogoncon@gmail.com>
2026-09-22 16:58:16 +0200

    update

--- T1/Componentes/src/Entrada.java
+++ T1/Componentes/src/Entrada.java
@@ -1,3 +1,16 @@
-public class Entrada {
 
-}
+    public static void main(String [] args) {
+
+        String nombre="Mario";
+        String apellido="González";
+        int DNI= 51178401;
+        char letra ='C';
+        double altura=1.74;
+        int edad = 28;
+
+        System.out.println("Nombre: "+nombre);
+        System.out.println("DNI: "+DNI+" con letra "+letra);
+        System.out.println("Altura: "+altura+" metros");
+        System.out.println("Edad "+edad+" años");
+    }
+
